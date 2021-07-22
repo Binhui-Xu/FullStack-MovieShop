3389d21fecd83aef03f8e9b059c756923ff6ce01
Whitney-XU <bhxu0530@gmail.com>
2021-07-22 13:36:23 -0400

    moviedetail

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -11,19 +11,21 @@ import { Moviecard } from 'src/app/shared/models/moviecard';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  movie!:Movie;
-  movieId!:number;
-  constructor(private router:ActivatedRoute,private movieService : MovieService) { }
-  //get the movie id from the URL and call the moiveservice, 
+  movie!: Movie;
+  id!: number;
+  constructor(private movieservice: MovieService, private route: ActivatedRoute) { }
+
   ngOnInit(): void {
-    this.router.paramMap.subscribe(
-      p=>{
-        this.movieId=+p.get('id');
-        this.movieService.getMovieDetails(this.movieId)
-        .subscribe(m=>{
-          this.movie=m;
+
+    this.route.paramMap.subscribe(p => {
+      this.id = +p.get('id')!;
+      console.log(this.id);
+      this.movieservice.getMovieDetails(this.id)
+        .subscribe(m => {
+          this.movie = m;
+          console.log(this.movie);
         })
-      }
-    )
+    });
   }
+
 }
